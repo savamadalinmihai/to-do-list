aa416bb15e1bf2bfef256c86b9d8365fffe27d9c
Sava Madalin <savamadalinmihai@gmail.com>
2020-02-12 14:47:28 +0200

    Added CRUD methods in task repository

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,6 +2,7 @@ package org.fasttrackit;
 
 import org.fasttrackit.persistance.TaskRepository;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -9,13 +10,22 @@ import java.time.LocalDate;
 public class App 
 {
     public static void main( String[] args ) throws SQLException {
+        // here we have this object in order to be able to access functions/methods inside it
         TaskRepository taskRepository= new TaskRepository();
 
+// this creates a new task
         CreateTaskRequest request = new CreateTaskRequest();
-
         request.setDescription("Learn JDBC");
         request.setDeadline(LocalDate.now().plusWeeks(1));
-
         taskRepository.createTask(request);
+
+// this updates a task
+//        UpdateTaskRequest request = new UpdateTaskRequest();
+//        request.setDone(true);
+//        taskRepository.updateTask(1, request);
+
+// this deletes a task
+//        taskRepository.deleteTask(1);
+
     }
 }
--- src/main/java/org/fasttrackit/persistance/TaskRepository.java
+++ src/main/java/org/fasttrackit/persistance/TaskRepository.java
@@ -1,12 +1,13 @@
 package org.fasttrackit.persistance;
 
+import org.fasttrackit.domain.Task;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskRepository {
 
@@ -14,8 +15,8 @@ public class TaskRepository {
         // preventing sql injection by avoiding concatenation and using prepared statement
         String sql = "INSERT INTO task (description, deadline) VALUES (?,?)";
 
-        try(Connection connection = DatabaseConfiguration.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             //here, we declare which parameters from (?,?) should be replaced with what data.
             preparedStatement.setString(1, request.getDescription());
@@ -24,6 +25,61 @@ public class TaskRepository {
             preparedStatement.executeUpdate();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void updateTask(long id, UpdateTaskRequest request) throws SQLException {
+        // preventing sql injection by avoiding concatenation and using prepared statement
+        String sql = "UPDATE task SET done = ? WHERE id = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            //here, we declare which parameters from (?,?) should be replaced with what data.
+            preparedStatement.setBoolean(1, request.isDone());
+            preparedStatement.setLong(2, id);
+
+            preparedStatement.executeUpdate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTask(long id) throws SQLException {
+        // preventing sql injection by avoiding concatenation and using prepared statement
+        String sql = "DELETE FROM task WHERE id = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            //here, we declare which parameters from (?,?) should be replaced with what data.
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Task> getTasks() throws IOException, SQLException {
+        // statement should be used only for no parameter queries.
+        String sql = "SELECT id, description, deadline, done FROM task";
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            List<Task> tasks = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Task task = new Task();
+                task.setId(resultSet.getLong("id"));
+                task.setDescription(resultSet.getString("description"));
+                task.setDeadline(resultSet.getDate("deadline").toLocalDate());
+                task.setDone(resultSet.getBoolean("done"));
+
+                tasks.add(task);
+            }
+            return tasks;
         }
     }
 }
